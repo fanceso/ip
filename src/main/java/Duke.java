906b6ec8466eb3e4b7fb537dbdff59f91e317be8
fanceso <yf144114@gmail.com>
2020-08-28 18:20:20 +0800

    Complete "level 0"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String LINE = "____________________________________________________________";
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String MESSAGE_GOODBYE = "Bye. Hope to see you again soon!";
+    private static final String COMMAND_EXIT_STRING = "bye";
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        String line = "____________________________________________________________";
-        System.out.println(line);
+        wellcomeMessage();
+        while (true) {
+            String userCommand = getUserInput();
+            executeCommand(userCommand);
+        }
+    }
+    static void UiDisplay(String content) {
+        System.out.println("    " + LINE);
+        System.out.println("    " + content);
+        System.out.println("    " + LINE);
+    }
+
+    private static void wellcomeMessage() {
+        System.out.println(LINE);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
-
+        System.out.println(LINE);
     }
+
+    private static void executeCommand(String userCommand) {
+        switch (userCommand) {
+        case COMMAND_EXIT_STRING:
+            exitProgram();
+        default:
+            UiDisplay(userCommand);
+        }
+    }
+
+    private static String getUserInput() {
+        String inputLine = SCANNER.nextLine();
+        return inputLine;
+    }
+
+    private static void exitProgram() {
+        UiDisplay(MESSAGE_GOODBYE);
+        System.exit(0);
+    }
+
+
 }

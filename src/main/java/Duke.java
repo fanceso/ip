2698be33d4aedc3fd403685afbe35c7547328f49
fanceso <yf144114@gmail.com>
2020-08-30 11:15:22 +0800

    Level 2 task completed. Added "listContent" and "addContent" method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
+
 public class Duke {
-    private static String LINE = "____________________________________________________________";
+    private static final String LINE = "____________________________________________________________";
+    private static final String INDENT = "    ";
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String MESSAGE_GOODBYE = "Bye. Hope to see you again soon!";
     private static final String COMMAND_EXIT_STRING = "bye";
+    private static final String COMMAND_LIST_STRING = "list";
+    private static final int Capacity = 50;
+    private static int contentCount = 0;
+    private static String[] contents = new String[Capacity];
 
     public static void main(String[] args) {
         wellcomeMessage();
@@ -13,10 +19,13 @@ public class Duke {
             executeCommand(userCommand);
         }
     }
+
     static void UiDisplay(String content) {
-        System.out.println("    " + LINE);
-        System.out.println("    " + content);
-        System.out.println("    " + LINE);
+        System.out.println(INDENT + LINE);
+        if (content != null) {
+            System.out.println(INDENT + content);
+        }
+        System.out.println(INDENT + LINE);
     }
 
     private static void wellcomeMessage() {
@@ -28,10 +37,14 @@ public class Duke {
 
     private static void executeCommand(String userCommand) {
         switch (userCommand) {
+        case COMMAND_LIST_STRING:
+            listProgram();
+            break;
         case COMMAND_EXIT_STRING:
             exitProgram();
+            break;
         default:
-            UiDisplay(userCommand);
+            addContent(userCommand);
         }
     }
 
@@ -45,5 +58,26 @@ public class Duke {
         System.exit(0);
     }
 
+    private static void listProgram() {
+        StringBuilder displayContent = new StringBuilder();
+        String content;
+        for (int i = 0; i < contents.length; i++) {
+            if (contents[i] != null ) {
+                if (contents[i + 1]  != null ) {
+                    displayContent.append(contents[i] + "\n" + INDENT);
+                }
+                else{
+                    displayContent.append(contents[i]);
+                }
+            }
+        }
+        String singleString = displayContent.toString();
+        UiDisplay(singleString);
+    }
 
+    private static void addContent(String inputContent) {
+        contents[contentCount] = inputContent;
+        contentCount++;
+        UiDisplay(inputContent);
+    }
 }
